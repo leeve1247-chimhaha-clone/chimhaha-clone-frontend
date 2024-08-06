33bd76affb8f7472106234471d5a8d6e0410726f
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-07 05:34:34 +0900

    Delete TailWind

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-
-export default {
-    content: ["./src/**/*.{html,js,ts,jsx,tsx}","./index.html"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-} satisfies Config
