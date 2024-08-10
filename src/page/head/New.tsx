512ca7f343855a5f065a47f1f4aca570447129ff
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-11 08:30:16 +0900

    feat(post): add get post api call

--- src/page/head/New.tsx
+++ src/page/head/New.tsx
@@ -1,7 +1,26 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { PostComponent, PostItem } from "../../component/PostComponent.tsx";
 
 export function New() {
-  return <><h1>New</h1>
-    <Link to={"post"}></Link>
-  </>;
+  const [data, setData] = useState<PostItem[]>([]);  // 데이터를 저장할 상태
+
+  useEffect(() => {
+    // useEffect 내부에 선언함으로써, (개발자들이) 재사용방지
+    async function fetchMeals() {
+      const axiosResponse = await axios.get("http://localhost:8080/posts")
+      setData(axiosResponse.data);
+      console.log(axiosResponse.data)
+    }
+    fetchMeals().then()
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {data.map((post) => (
+        <PostComponent key={post.title} post={post} />
+      ))}
+    </div>
+  );
 }
