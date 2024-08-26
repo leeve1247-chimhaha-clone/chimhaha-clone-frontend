7a70136f967d717e2faa0fdc254daed72cabae0a
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-27 04:35:40 +0900

    Add pkce Feature

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,14 +1,59 @@
-export function Login(){
+import { useAuth } from "react-oidc-context";
+import axios from "axios";
+
+export function Login() {
+  const auth = useAuth();
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+      return <div>Signing you in...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
     return (
-      <>
-        <div>
-          <h1>로그인</h1>
-          <form>
-            <input type="text" placeholder="아이디" />
-            <input type="password" placeholder="비밀번호" />
-            <button>로그인</button>
-          </form>
-        </div>
-      </>
+      <div>
+        {auth.user?.access_token}
+        Hello {auth.user?.profile.sub}{" "}
+        <button
+          onClick={() => {
+            console.log(auth?.user?.access_token);
+            void auth.removeUser();
+          }}
+        >
+          Log out
+        </button>
+        <button onClick={() => getList()}>Get List</button>
+      </div>
     );
+  }
+
+  async function getList() {
+    try {
+      const myData = await axios.get(
+        "http://192.168.1.14:8080/myAccount?username=userC",
+        {
+          headers: {
+            Authorization: `Bearer ${auth.user?.access_token}`,
+          },
+        },
+      );
+      console.log(myData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <>
+      <button onClick={() => void auth.signinRedirect()}>Log in</button>
+    </>
+  );
 }
