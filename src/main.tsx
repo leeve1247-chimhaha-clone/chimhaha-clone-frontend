1db8285d8d0d4fbb280d44ad4608f61cc5ec9e33
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-27 04:35:40 +0900

    feat(auth): add pkce oauth2 feature

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
+import { AuthProvider } from "react-oidc-context";
+import {CData} from "./credential/data.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider
+      authority={CData.authority}
+      client_id={CData.client_id}
+      redirect_uri={CData.redirect_uri}
+    >
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 );
