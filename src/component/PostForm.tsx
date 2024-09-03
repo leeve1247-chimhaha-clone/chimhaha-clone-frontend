46ef2fd2fb52b693396941b7706d48c865c8f46a
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-04 06:37:59 +0900

    Update PostForm Working add Token

--- src/component/PostForm.tsx
+++ src/component/PostForm.tsx
@@ -3,12 +3,16 @@ import Quill from "quill";
 import axios from "axios";
 import { WYSIWYGEditor } from "./WYSIWYGEditor.tsx";
 import cssStyle from "./PostForm.module.css";
+import { RData } from "../credential/data.ts";
+import { useAuth } from "react-oidc-context";
 
 export function PostForm() {
   const quillRef = useRef<Quill>(null);
   const title = useRef<HTMLInputElement>(null);
+  const auth = useAuth();
 
   const saveContent = async () => {
+    console.log("heyhey")
     if (quillRef.current) {
       const delta = quillRef.current.getContents();
       const titleText = title.current?.value;
@@ -16,18 +20,22 @@ export function PostForm() {
         category: "BEST",
         content: delta,
         title: titleText,
-        user: "UserA",
+        user: auth?.user?.profile?.sub,
       });
+      console.log(auth?.user?.profile?.sub)
       console.log(deltaJson);
-
+      console.log(titleText)
+      console.log(auth.user?.access_token)
       try {
-        await axios.post("http://localhost:8080/save", {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          content: deltaJson,
-        });
+        await axios.post(RData.baseUrl+"/save", deltaJson, {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${auth.user?.access_token}`,
+            }
+          }
+        );
         alert("Content saved successfully!");
+        console.log("goooooood")
       } catch (error) {
         console.error("Error saving content:", error);
         alert("Failed to save content.");
@@ -38,7 +46,7 @@ export function PostForm() {
   return (
     <>
       <div className={cssStyle.title}> 카테고리 </div>
-      <input className={cssStyle.title} placeholder={"안녕? 난 제목이라고 해"}/>
+      <input className={cssStyle.title} placeholder={"안녕? 난 제목이라고 해"} ref={title} type="text"/>
       <div>
         <WYSIWYGEditor ref={quillRef} />
         <button onClick={saveContent}>Save Content</button>
--- src/page/head/New.tsx
+++ src/page/head/New.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { PostComponent, PostItem } from "../../component/PostComponent.tsx";
 import { NavLink } from "react-router-dom";
 import { ROUTES } from "../../router.tsx";
+import { RData } from "../../credential/data.ts";
 
 export function New() {
   const [data, setData] = useState<PostItem[]>([]);  // 데이터를 저장할 상태
@@ -10,9 +11,8 @@ export function New() {
   useEffect(() => {
     // useEffect 내부에 선언함으로써, (개발자들이) 재사용방지
     async function fetchMeals() {
-      const axiosResponse = await axios.get("http://localhost:8080/posts")
+      const axiosResponse = await axios.get(RData.baseUrl+"/posts")
       setData(axiosResponse.data);
-      console.log(axiosResponse.data)
     }
     fetchMeals().then()
   }, []);
