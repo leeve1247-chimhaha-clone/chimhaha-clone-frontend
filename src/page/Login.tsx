3bc8e3222ae36263e138287c98c87958cc67bc62
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-05 12:31:31 +0900

    feat(user): update nickname initialization

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,7 +1,27 @@
 import { useAuth } from "react-oidc-context";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { RData } from "../credential/data.ts";
 
 export function Login() {
   const auth = useAuth();
+  const [nickName, setNickName] = useState<string>("");
+
+  useEffect(() => {
+    // useEffect 내부에 선언함으로써, (개발자들이) 재사용방지
+    async function fetchMeals() {
+      const axiosResponse = await axios.get(RData.baseUrl + "/getMyNickName", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.user?.access_token}`,
+        },
+      });
+      const nameData = axiosResponse.data;
+      setNickName(nameData);
+    }
+    fetchMeals().then();
+  }, [auth?.user?.profile?.sub]);
+
   switch (auth.activeNavigator) {
     case "signinSilent":
       return <div>Signing you in...</div>;
@@ -19,29 +39,34 @@ export function Login() {
 
   if (auth.isAuthenticated) {
     return (
-      <div>
-        Hello,
-        {auth?.user?.profile?.sub}
-        <button
-          onClick={() => {
-            console.log(auth?.user?.access_token);
-            void auth.removeUser();
-          }}
-        >
-          Log out
-        </button>
-        <button onClick={() => deleteUser()}>Delete User from UserEntityRepository</button>
-      </div>
+      <>
+        <div>{nickName}, 안녕하신지요?</div>
+        <div>
+          <button
+            onClick={() => {
+              console.log(auth?.user?.access_token);
+              void auth.removeUser();
+            }}
+          >
+            Log out
+          </button>
+          <button onClick={() => deleteUser()}>
+            Delete User from UserEntityRepository
+          </button>
+        </div>
+      </>
     );
   }
 
-  async function deleteUser(){
-    console.log("delete user")
+  async function deleteUser() {
+    console.log("delete user");
   }
 
   return (
     <>
-      <button onClick={() => void auth.signinRedirect()}>Log in By Github</button>
+      <button onClick={() => void auth.signinRedirect()}>
+        Log in By Github
+      </button>
       <button onClick={() => void auth.signinRedirect()}>Log in</button>
     </>
   );
