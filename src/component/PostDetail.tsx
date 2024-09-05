e95ee64605f311ee2d3662b0f9c17c10e73eba51
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-05 10:37:58 +0900

    Add Upload Comment Function

--- src/component/PostDetail.tsx
+++ src/component/PostDetail.tsx
@@ -7,15 +7,46 @@ import Quill from "quill";
 import Delta from "quill-delta";
 import { useRef } from "react";
 import { WYSIWYGEditor } from "./WYSIWYGEditor.tsx";
+import { CommentEditor } from "./CommentEditor.tsx";
+import { useAuth } from "react-oidc-context";
 
 export function PostDetail() {
   const data = useLoaderData() as PostDetailProps;
   const quillRef = useRef<Quill>(null);
+  const commentRef = useRef<Quill>(null);
+  const auth = useAuth()
+
+  const saveComment = async () => {
+    if (quillRef.current) {
+      const delta = quillRef.current.getContents();
+      const deltaJson = JSON.stringify({
+        content: delta,
+        postId: data.postId
+      });
+      try {
+        await axios.post(RData.baseUrl+"/save/comment", deltaJson, {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${auth.user?.access_token}`,
+            }
+          }
+        );
+        alert("Comment saved successfully!");
+      } catch (error) {
+        console.error("Error saving content:", error);
+        alert("Failed to save content.");
+      }
+    }
+  };
+
   return (
     <>
       <h1>인기글</h1>
       <h1>디테일</h1>
       <WYSIWYGEditor ref={quillRef} defaultValue={data.content} />
+      <h1>이하 댓글 창</h1>
+      <CommentEditor ref={commentRef}/>
+      <button onClick={saveComment}>댓글 입력</button>
     </>
   );
 }
