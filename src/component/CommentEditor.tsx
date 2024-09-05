b88eff3dfaedcc631d324a8eca1e72977955d9f8
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-05 10:38:15 +0900

    Create CommentEditor.tsx

---
+++ src/component/CommentEditor.tsx
@@ -0,0 +1,29 @@
+import { forwardRef, useEffect } from "react";
+import "./WYSIWYGEditor.css";
+import Quill from "quill";
+import Delta from "quill-delta";
+
+export const CommentEditor = forwardRef<Quill, {defaultValue? : Delta}>(
+  ({}, quillRef) => {
+    useEffect(() => {
+      if (quillRef === null || typeof quillRef === "function") return;
+      const container = document.getElementById("comment-editor") as HTMLElement;
+      if (container && !quillRef.current) {
+          quillRef.current = new Quill(container, {
+            modules: {
+              toolbar: false,
+            },
+            theme: "snow",
+            formats: []
+          });
+
+      }
+    }, [quillRef]);
+
+    return <>
+      <h1>comment edit Start</h1>
+      <div id="comment-editor" style={{ height: "100px" }} />
+      <h1>comment edit End</h1>
+    </>
+  },
+);
