973d53a96533c974b264d5c12dd5288a66cd5571
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-08 07:34:04 +0900

    [게시글쓰기] 작성후 게시글로 리다이렉트

--- src/component/PostForm.tsx
+++ src/component/PostForm.tsx
@@ -4,20 +4,22 @@ import { WYSIWYGEditor } from "./WYSIWYGEditor.tsx";
 import cssStyle from "./PostForm.module.css";
 import { useAuth } from "react-oidc-context";
 import { savePost } from "../utils/savePost.ts";
-
+import { useNavigate } from "react-router-dom";
 
 export function PostForm() {
   const quillRef = useRef<Quill>(null);
   const title = useRef<HTMLInputElement>(null);
   const auth = useAuth();
+  const navigate = useNavigate();
 
   async function sendPostContent() {
-    await savePost({
+    const axiosResponse = await savePost({
       quill: quillRef.current,
       titleText: title.current?.value,
       user: auth?.user?.profile?.sub,
       access_token: auth.user?.access_token,
     });
+    navigate("/new/" + axiosResponse?.data)
   }
 
   return (
