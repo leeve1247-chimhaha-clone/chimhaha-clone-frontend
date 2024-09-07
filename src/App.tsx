abba66b398512773d633c996f700deb214159600
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-08 08:13:17 +0900

    [게시글쓰기] Quill Editor 모듈 최상단으로 옮김

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import { HeaderMain } from "./component/HeaderMain.tsx";
 import { HeaderSub } from "./component/HeaderSub.tsx";
 import { HeaderNav } from "./component/HeaderNav.tsx";
 import { Outlet } from "react-router";
+import Quill from "quill";
+import { ImageBlot } from "./utils/ImageBlot.ts";
 
 export function App() {
+  const RegisteredBlot = Quill.import('blots/block/embed');
+  if (RegisteredBlot === ImageBlot) {
+  } else {
+    Quill.register(ImageBlot);
+  }
   return (
     <>
       <header>
