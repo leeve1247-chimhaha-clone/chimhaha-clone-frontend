ae842f4e14e10b8b448e189a6d70d19f56853aab
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-07 21:15:39 +0900

    feat(redux): initialize redux store

---
+++ src/utils/redux/store.ts
@@ -0,0 +1,32 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+
+export interface StateProps {
+  token: string
+  nickName: string
+}
+const initialState : StateProps = {
+  token : "",
+  nickName : ""
+}
+const reducers = {
+  setToken: (state: StateProps, action: { payload: string }) => {
+    state.token = action.payload
+  },
+  setNickName: (state: StateProps, action: { payload: string }) => {
+    state.nickName = action.payload
+  },
+}
+
+const stateSlice = createSlice({
+  name: 'token',
+  initialState,
+  reducers
+})
+
+export const store = configureStore({
+  reducer: stateSlice.reducer
+})
+export const { setToken, setNickName } = stateSlice.actions
+store.subscribe(()=>{console.log(store.getState())})
+
+console.log(store.getState())
