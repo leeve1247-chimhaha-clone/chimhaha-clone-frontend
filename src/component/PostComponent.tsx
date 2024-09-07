caa013a874145adb29bd9bf9593514813ef00910
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-08 06:50:51 +0900

    feat(thumbnail): add thumbnail to post component

--- src/component/PostComponent.tsx
+++ src/component/PostComponent.tsx
@@ -9,6 +9,7 @@ import {
   faThumbsUp,
 } from "@fortawesome/free-regular-svg-icons";
 import { NavLink } from "react-router-dom";
+import { ImageThumbNail } from "./ImageThumbNail.tsx";
 
 export interface PostItem {
   title: string;
@@ -21,34 +22,42 @@ export interface PostItem {
   createdDate: string;
   likes: number;
   postId?: number;
+  titleImageId?: string;
 }
 
 export function PostComponent({ post }: { post: PostItem }) {
   const postId = post.postId;
   return (
-    <NavLink to={ postId?.toString() ?? ""} className={cssClass.postContainer}>
+    <NavLink to={postId?.toString() ?? ""} className={cssClass.postContainer}>
+      {post.titleImageId ? (
+        <ImageThumbNail
+          imageId={post.titleImageId}
+          className={cssClass.image}
+        ></ImageThumbNail>
+      ) : (
         <FontAwesomeIcon className={cssClass.image} icon={faFileLines} />
-        <div className={cssClass.rightSection}>
-          <div className={cssClass.firstLine}>
-            <Category className={cssClass.category} category={post.category} />
-            <div className={cssClass.title}>{post.title}</div>
-          </div>
-          <div className={cssClass.secondLine}>
-            <div>{post.username}</div>
-            <FontAwesomeIcon className={cssClass.dot} icon={faCircle} />
-            <CreatedDate date={post.createdDate} />
-            <FontAwesomeIcon className={cssClass.dot} icon={faCircle} />
-            <FontAwesomeIcon icon={faEye} />
-            <div>{post.views}</div>
-            {post.likes > 0 && (
-              <>
-                <FontAwesomeIcon className={cssClass.dot} icon={faCircle} />
-                <FontAwesomeIcon className={cssClass.likes} icon={faThumbsUp} />
-                <div className={cssClass.likes}>{post.likes}</div>
-              </>
-            )}
-          </div>
+      )}
+      <div className={cssClass.rightSection}>
+        <div className={cssClass.firstLine}>
+          <Category className={cssClass.category} category={post.category} />
+          <div className={cssClass.title}>{post.title}</div>
         </div>
+        <div className={cssClass.secondLine}>
+          <div>{post.username}</div>
+          <FontAwesomeIcon className={cssClass.dot} icon={faCircle} />
+          <CreatedDate date={post.createdDate} />
+          <FontAwesomeIcon className={cssClass.dot} icon={faCircle} />
+          <FontAwesomeIcon icon={faEye} />
+          <div>{post.views}</div>
+          {post.likes > 0 && (
+            <>
+              <FontAwesomeIcon className={cssClass.dot} icon={faCircle} />
+              <FontAwesomeIcon className={cssClass.likes} icon={faThumbsUp} />
+              <div className={cssClass.likes}>{post.likes}</div>
+            </>
+          )}
+        </div>
+      </div>
     </NavLink>
   );
 }
