b7add31c7b27956f7a59822a1153b847bc57c953
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-17 08:28:22 +0900

    [댓글 기능] 댓글 수정 사항에 대한 Editor 변경

--- src/component/comment/CommentUpdateEditor.tsx
+++ src/component/comment/CommentUpdateEditor.tsx
@@ -3,12 +3,17 @@ import "../WYSIWYGEditor.css";
 import Quill from "quill";
 import Delta from "quill-delta";
 
-export const CommentUpdateEditor = forwardRef<Quill, {defaultValue? : Delta, commentId: string}>(
-  ({commentId, defaultValue}, quillRef) => {
+interface CommentUpdateEditorProps {defaultValue? : Delta, commentId: string, editState?: boolean, removedState: boolean}
+
+
+export const CommentUpdateEditor = forwardRef<Quill,CommentUpdateEditorProps >(
+  ({commentId, defaultValue, editState, removedState}, quillRef) => {
     useEffect(() => {
       if (quillRef === null || typeof quillRef === "function") return;
       const container = document.getElementById("comment-" + commentId) as HTMLElement;
-      if (container && !quillRef.current) {
+      // 1. read // removed
+      if (container && !editState) {
+        if (!editState) {
           quillRef.current = new Quill(container, {
             readOnly: true,
             modules: {
@@ -17,9 +22,20 @@ export const CommentUpdateEditor = forwardRef<Quill, {defaultValue? : Delta, com
             theme: "snow",
             formats: []
           });
-          if (defaultValue) quillRef.current.setContents(defaultValue)
+          if (defaultValue !== undefined) quillRef.current.setContents(defaultValue)
+        }
+        // 2. update
+      } else if (container && quillRef.current && editState){
+        quillRef.current = new Quill(container, {
+          readOnly: false,
+          modules:{
+            toolbar: false,
+          },
+          theme: "snow",
+          formats: []
+        });
       }
-    }, [quillRef]);
+    }, [quillRef,editState, removedState]);
 
     return <>
       <div id={`comment-${commentId}`}/>
