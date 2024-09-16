91f7f5550e257bfb991df924894fa424ebdd038d
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-17 08:29:30 +0900

    feat(comment): update comment crud requests and like component

--- src/component/comment/CommentComponent.tsx
+++ src/component/comment/CommentComponent.tsx
@@ -1,9 +1,15 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Quill from "quill";
 import { CommentUpdateEditor } from "./CommentUpdateEditor.tsx";
 import { CommentEditorComponent } from "./CommentEditorComponent.tsx";
 import cssClass from "./CommentComponent.module.css";
 import Delta from "quill-delta";
+import { useAuth } from "react-oidc-context";
+import {
+  deleteComment,
+  likeComment,
+  updateComment,
+} from "../../utils/saveComment.ts";
 
 interface CommentComponentProps {
   postId: string;
@@ -15,25 +21,108 @@ export interface CommentProps {
   username?: string;
   content: Delta;
   id: string;
+  likes: string;
   children?: CommentProps[];
 }
 
-export function CommentComponent({ postId, comment }: CommentComponentProps) {
+export function CommentComponent({
+  postId,
+  comment: initComment,
+}: CommentComponentProps) {
   const commentRef = useRef<Quill>(null);
+  const auth = useAuth();
+  const [comment, setComment] = useState(initComment);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isRemoved, setIsRemoved] = useState(false);
+
+  async function likeThisComment() {
+    if (!auth.isAuthenticated) return;
+    const axiosResponse = await likeComment({
+      commentId: comment.id,
+      access_token: auth.user?.access_token!,
+    });
+    setComment((prevData) => ({
+      ...prevData,
+      likes: axiosResponse.data,
+    }));
+  }
+
+  function handleDataReceived(data: CommentProps) {
+    setComment((prevData) => ({
+      ...prevData,
+      children: [...(prevData.children || []), data],
+    }));
+  }
+
+  function editThisComment() {
+    commentRef.current;
+    setIsEdit(true);
+  }
+
+  function cancelEditThisComment() {
+    setIsEdit(false);
+  }
+
+  async function sendEditedComment() {
+    const axiosResponse = await updateComment({
+      access_token: auth.user?.access_token!,
+      comment: commentRef.current?.getContents(),
+      commentId: comment.id,
+    });
+
+    if (axiosResponse?.data.content !== null) {
+      setComment((prevState) => ({
+        ...prevState,
+        content: axiosResponse?.data.content,
+      }));
+      setIsEdit(false);
+    }
+  }
+
+  async function deleteThisComment() {
+    const axiosResponse = await deleteComment({
+      access_token: auth.user?.access_token!,
+      commentId: comment.id,
+    });
+    if (axiosResponse?.data.id !== null) {
+      setComment((prevState) => ({
+        ...prevState,
+        content: axiosResponse?.data.content,
+      }));
+    }
+    setIsRemoved(true);
+  }
+
   return (
     <div>
       <CommentUpdateEditor
         ref={commentRef}
         defaultValue={comment.content}
         commentId={comment.id}
+        editState={isEdit}
+        removedState={isRemoved}
       />
-      <div className={cssClass.buttons}>
-        <button>수정</button>
-        <button>삭제</button>
-      </div>
-        <CommentEditorComponent postId={postId} commentId={comment.id}>
-          답글 달기
-        </CommentEditorComponent>
+      {!isEdit && !isRemoved && (
+        <div className={cssClass.buttons}>
+          <div>{comment.likes}</div>
+          <button onClick={likeThisComment}>좋아요</button>
+          <button onClick={editThisComment}>수정</button>
+          <button onClick={deleteThisComment}>삭제</button>
+        </div>
+      )}
+      {isEdit && !isRemoved && (
+        <div className={cssClass.buttons}>
+          <button onClick={cancelEditThisComment}>취소</button>
+          <button onClick={sendEditedComment}>입력</button>
+        </div>
+      )}
+      <CommentEditorComponent
+        onDataReceived={handleDataReceived}
+        postId={postId}
+        commentId={comment.id}
+      >
+        답글 달기
+      </CommentEditorComponent>
 
       <div className={cssClass.termToLeft}>
         {comment.children !== undefined && comment.children?.length !== 0 ? (
