9a603827ba9d8c7ecb547ff46d9c0090e10c1008
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-15 15:50:28 +0900

    build(deps): install react-query

--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,27 @@ import { CData } from "../credential/data.ts";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.tsx";
 import { AppRouterProvider } from "./router/AppRouterProvider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AuthProvider
-        authority={CData.authority}
-        client_id={CData.client_id}
-        redirect_uri={CData.redirect_uri}
-        onSigninCallback={async () => {
-          window.history.replaceState({}, document.title, window.location.pathname);
-        }}
-        post_logout_redirect_uri={CData.redirect_uri}
-        automaticSilentRenew={true}
-      >
-        <AppRouterProvider />
-      </AuthProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <AuthProvider
+          authority={CData.authority}
+          client_id={CData.client_id}
+          redirect_uri={CData.redirect_uri}
+          onSigninCallback={async () => {
+            window.history.replaceState({}, document.title, window.location.pathname);
+          }}
+          post_logout_redirect_uri={CData.redirect_uri}
+          automaticSilentRenew={true}
+        >
+          <AppRouterProvider />
+        </AuthProvider>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
