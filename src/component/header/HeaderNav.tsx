2331328af9fcf56d50d136c253b7a3b434382345
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-17 12:21:07 +0900

    Update HeaderNav.tsx

--- src/component/header/HeaderNav.tsx
+++ src/component/header/HeaderNav.tsx
@@ -2,9 +2,26 @@ import cssClass from "./HeaderNav.module.css";
 import { setHeaderDropDownStatus } from "../../redux/dropDown/dropDownSlice.tsx";
 import { useDispatch } from "react-redux";
 import { HeaderLevelZero } from "./refactor/HeaderLevelZero.tsx";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { CData } from "../../../credential/data.ts";
+
+async function myFunction() {
+  return axios.get(CData.local_backend + "/post-categories")
+    .then(
+      (response) => {
+        console.log("Data fetched successfully:", response.data);
+        return JSON.stringify(response.data);
+      }
+    )
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
 
 export function HeaderNav() {
   const dispatch = useDispatch();
+  const {data, error, isLoading, isError} = useQuery({queryKey: ['hello'], queryFn: myFunction});
   return (
     <>
       <div className={cssClass.navContainer}>
