3d89ee273d562d3ba85f6809d094f7c006af2ead
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-18 00:02:54 +0900

    feat: add test

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,39 @@ import { HeaderNav } from "./component/header/HeaderNav.tsx";
 import HeaderSub from "./component/header/HeaderSub.tsx";
 import { HeaderSubCategory } from "./component/header/HeaderSubCategory.tsx";
 import HeaderDropDown from "./component/header/dropdown/HeaderDropDown.tsx";
+import axios from "axios";
+import { useState } from "react";
+import { CData } from "../credential/data.ts";
+import file from "../public/vite.svg";
 
 export function App() {
+  const [state, setState] = useState();
+
+  async function getPresignedUrl() {
+    axios
+      .get(CData.local_backend+"/get/presigned-url")
+      .then((response) => {
+        console.log(response.data);
+        setState(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  async function postPresignedUrl() {
+    axios.put(CData.local_image_uri+state, file, {
+      headers:{
+        "Content-Type": "image/svg+xml"
+      }
+    })
+      .then((response) => {
+        console.log(response.data);
+      }).catch((error) => {
+        console.error(error);
+    })
+  }
+
   return (
     <>
       <Header>
@@ -27,6 +58,11 @@ export function App() {
       </Header>
       <main>
         <Outlet />
+        <div>
+          <button onClick={getPresignedUrl}>Get Presigned URL</button>
+          <div>{state}</div>
+          <button onClick={postPresignedUrl}>Post presigned Image</button>
+        </div>
       </main>
       <footer>
         <p>© 2021. All rights reserved.</p>
