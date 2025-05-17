6473b9ab95d46647ce485a87a90274be3afcebf9
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-17 22:36:32 +0900

    feat: add default components

--- src/component/header/NavAllPosts.tsx
+++ src/component/header/NavAllPosts.tsx
@@ -1,12 +1,15 @@
 import { useDispatch } from "react-redux";
 import { setHeaderDropDownStatus } from "../../redux/dropDown/dropDownSlice.tsx";
+import { useNavigate } from "react-router-dom";
 
 export function NavAllPosts() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   return (
     <button
       onClick={() => {
         dispatch(setHeaderDropDownStatus(""));
+        navigate("/All")
       }}
     >
       전체글
