a7464d6b3c7d4b2481b812082a706770d9330a5f
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-26 12:03:44 +0900

    feat(redux): integrate redux state management

--- src/component/comment/redux/submitPostReducers.tsx
+++ src/component/comment/redux/submitPostReducers.tsx
@@ -1,23 +1,9 @@
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-import type { SubmitPostStateProps } from "./SubmitPostStateProps.tsx";
-import type { SerializedEditorState } from "lexical";
+import type { CommentComponentStateProps } from "./CommentComponentStateProps.tsx";
 
-export const submitPostReducers = {
-  setTitle: (state: SubmitPostStateProps, action: PayloadAction<string>) => {
-    state.title = action.payload;
+export const commentComponentReducer = {
+  setCommentPage: (state: CommentComponentStateProps, action: PayloadAction<number>) => {
+    state.commentPage = action.payload;
   },
-  setContent: (state: SubmitPostStateProps, action: PayloadAction<SerializedEditorState>) => {
-    state.content = action.payload;
-  },
-  setCategory: (state: SubmitPostStateProps, action: PayloadAction<string>) => {
-    state.category = action.payload;
-  },
-
-  startSubmitPost: (state: SubmitPostStateProps) => {
-    state.run = true;
-  },
-  stopSubmitPost: (state: SubmitPostStateProps) => {
-    state.run = false;
-  }
 };
