07b746dc0bfaf388dff638f505197ede75436534
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-26 12:47:01 +0900

    refactor: migrate state management to redux

--- src/component/comment/buttons/CommentPageButtons.tsx
+++ src/component/comment/buttons/CommentPageButtons.tsx
@@ -1,16 +1,18 @@
 import style from "../CommentComponent.module.css";
+import { useDispatch } from "react-redux";
+import { setCommentPage } from "../redux/commentComponentSlice.tsx";
 
-export function CommentPageButtons({ pageSize, handleCommentPage }: {
-  pageSize: number;
-  handleCommentPage?: (pageNum: number) => void
-}) {
-  if (handleCommentPage === undefined) return <></>;
+export function CommentPageButtons({ pageSize }: { pageSize: number}) {
+  const dispatch = useDispatch();
+
   return (
     <div className={style.commentPageButtons}>
       {Array.from({ length: pageSize }).map((_, index) => (
-        <button key = {index} className={style.commentPageButton}
+        <button
+          key={index}
+          className={style.commentPageButton}
           onClick={() => {
-            handleCommentPage(index + 1);
+            dispatch(setCommentPage(index + 1));
           }}
         >
           {index + 1}
