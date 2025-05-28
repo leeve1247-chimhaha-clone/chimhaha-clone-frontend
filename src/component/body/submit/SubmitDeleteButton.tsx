049796b386f2c55f5e01c9e9c8b0883ce8eaf027
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-28 12:48:20 +0900

    feat(post): update post delete functionality

---
+++ src/component/body/submit/SubmitDeleteButton.tsx
@@ -0,0 +1,31 @@
+import { CData } from "../../../../credential/data.ts";
+import axios from "axios";
+import { useAuth } from "react-oidc-context";
+import { useMatches, useParams } from "react-router";
+import { useNavigate } from "react-router-dom";
+
+export function SubmitDeleteButton() {
+  const auth = useAuth();
+  const { postId } = useParams();
+  const matches = useMatches();
+  const category = matches[1].pathname.substring(1, matches[1].pathname.length);
+  const navigate = useNavigate();
+
+  async function handleButton() {
+    const jsonData = JSON.stringify({
+      postId: postId,
+    });
+    await axios
+      .post<number>(CData.local_backend + "/delete", jsonData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.user?.access_token}`,
+        },
+      })
+      .then((res) => res.data)
+      .catch(() => undefined);
+    navigate("/" + category);
+  }
+
+  return <button onClick={handleButton}>삭제</button>;
+}
