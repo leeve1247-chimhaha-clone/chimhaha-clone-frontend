cf12c9fa02088f2a548874d9af67f60aa0221856
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-28 13:57:46 +0900

    refactor: change directory name component-> components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import { Header } from "./component/header/Header.tsx";
+import { Header } from "./components/header/Header.tsx";
 
 export interface presignedUrlProps {
   url: string;
