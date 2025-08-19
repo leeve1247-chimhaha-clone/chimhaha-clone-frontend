0d3f1563e47965f059e700b88d63f5faf7b71004
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-08-19 14:49:38 +0900

    feat: update by AI but doesn't work

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,13 +2,11 @@ import styles from "./Header.module.css";
 import { HeaderSubCategory } from "./sub/HeaderSubCategory.tsx";
 import { HeaderMain } from "./main/HeaderMain.tsx";
 import { HeaderSub } from "./sub/HeaderSub.tsx";
-import HeaderDropDown from "./dropdown/HeaderDropDown.tsx";
 
 export function Header() {
   return (
     <div className={styles.container}>
       <HeaderMain />
-      <HeaderDropDown />
       <HeaderSub category={HeaderSubCategory.favorite} />
       <HeaderSub category={HeaderSubCategory.recent} />
     </div>
