e52fffd3d7d5da1d799aa5fe3d8e07cb60007246
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-11-18 01:33:47 +0900

    fix(post-list): remove link for debugging

--- src/components/post/list/YoutubeCard/PostListYoutubeCards.tsx
+++ src/components/post/list/YoutubeCard/PostListYoutubeCards.tsx
@@ -3,8 +3,8 @@ import { PostListYoutubeCard } from "./PostListYoutubeCard.tsx";
 
 export function PostListYoutubeCards() {
   return <div className={styles.youtubeCardList}>
-    <PostListYoutubeCard title={"최신 침착맨"} link={"https://www.youtube.com/embed/dQw4w9WgXcQ"} />
-    <PostListYoutubeCard title={"최신 침착맨 게임"} link={"https://www.youtube.com/embed/dQw4w9WgXcQ"} />
-    <PostListYoutubeCard title={"최신 생방송"} link={"https://www.youtube.com/embed/dQw4w9WgXcQ"} />
+    <PostListYoutubeCard title={"최신 침착맨"} link={""} />
+    <PostListYoutubeCard title={"최신 침착맨 게임"} link={""} />
+    <PostListYoutubeCard title={"최신 생방송"} link={""} />
   </div>;
 }
