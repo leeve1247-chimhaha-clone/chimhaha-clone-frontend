e5a1d45ecf867a1059e6f8647c81ca4ab11ef98d
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-11-18 01:45:06 +0900

    refactor(post-list): update youtube card in post list

--- src/components/post/list/YoutubeCard/PostListYoutubeCard.tsx
+++ src/components/post/list/YoutubeCard/PostListYoutubeCard.tsx
@@ -1,21 +1,25 @@
 import styles from "./PostListYoutubeCard.module.css";
-import iconUrl from '../../../../../public/youtube_icon.svg'
+import iconUrl from "../../../../../public/youtube_icon.svg";
 import { useWindowSize } from "../../../../context/window-size/UseWindowSize.tsx";
 
 export function PostListYoutubeCard({ title, link }: { title: string, link: string }) {
   const { width } = useWindowSize();
   return <div className={styles.youtubeCard}>
     <div className={styles.row}>
-      <img src={iconUrl} alt={"youtube_icon"}/>
+      <img src={iconUrl} alt={"youtube_icon"} />
       <div className={styles.youtubeCard__title}>{title}</div>
     </div>
-    <iframe className={styles.youtubeCard__body}
-            width={width > 800 ? width / 4 : 270}
-            height={width > 800 ? width / 4 / 16 * 9 : 180}
-            src={link}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen>
-    </iframe>
+    {link == "" ?
+      <div className={styles.youtubeCard__title}>None</div>
+      :
+      <iframe className={styles.youtubeCard__body}
+              width={width > 800 ? width / 4 : 270}
+              height={width > 800 ? width / 4 / 16 * 9 : 180}
+              src={link}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen>
+      </iframe>}
+
   </div>;
 }
