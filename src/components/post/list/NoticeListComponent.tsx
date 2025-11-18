7c446222cd054152db7cb7755c39109b0049686e
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-11-19 03:38:59 +0900

    fix: 카테고리 게시판에 게시되는 공지사항의 리스트 높이 조정
    -카드로 구분

--- src/components/post/list/NoticeListComponent.tsx
+++ src/components/post/list/NoticeListComponent.tsx
@@ -6,7 +6,11 @@ import axios from "axios";
 import { CData } from "../../../../credential/data.ts";
 import { NoticeCard } from "./NoticeCard/NoticeCard.tsx";
 
-export function NoticeListComponent() {
+interface NoticeListComponentProps {
+  long?: boolean;
+}
+
+export function NoticeListComponent({ long }: NoticeListComponentProps) {
   async function fetchNoticeList(): Promise<PostProps[] | undefined> {
     return axios
       .get<PostProps[]>(CData.local_backend + "/posts?category=notice")
@@ -18,20 +22,20 @@ export function NoticeListComponent() {
         return undefined;
       });
   }
-  const { data: notices, error: notices_error } = useQuery({ queryKey: queryKeys.NoticeList, queryFn: fetchNoticeList });
+
+  const { data: notices, error: notices_error } = useQuery({
+    queryKey: queryKeys.NoticeList,
+    queryFn: fetchNoticeList
+  });
 
   if (notices_error) return <div>Error: {notices_error.message}</div>;
   if (notices === undefined) return <div>No data</div>;
 
   return <>
-    <div className={styles.row}>
-      <h2 className={styles.h2}>공지</h2> <h2 className={styles.h2mark}>!</h2>
-    </div>
     <div className={styles.noticeHead}>
       {notices.map((notice, index) => (
-        <NoticeCard key={index} post={notice} />
+        <NoticeCard long={long} key={index} post={notice} />
       ))}
     </div>
-
   </>;
 }
