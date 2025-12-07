b2702ad95af4e8cb62c1897144ab93141abf53a3
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-12-08 08:44:10 +0900

    feat: add temp presigned post

--- src/components/post/submit/PostSubmitPostButton.tsx
+++ src/components/post/submit/PostSubmitPostButton.tsx
@@ -1,5 +1,5 @@
 import type { LexicalEditor } from "lexical";
-import { type RefObject } from "react";
+import { type BaseSyntheticEvent, type RefObject, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { CData } from "../../../../credential/data.ts";
@@ -14,6 +14,15 @@ import { clearImageSrcInEditorState } from "./functions/clearImageSrcInEditorSta
 import { isEmpty } from "./functions/isEmpty.ts";
 import type { RootState } from "../../../redux/store.tsx";
 
+interface PresignedPostProps {
+  url: string;
+  fields: {
+    key: string;
+    policy: string;
+    [key:string]: string
+  };
+}
+
 export function PostSubmitPostButton({ ref }: { ref: RefObject<LexicalEditor | undefined> }) {
   const selector = useSelector((state: RootState) => state.submitPostStatus);
   const dispatch = useDispatch();
@@ -24,6 +33,8 @@ export function PostSubmitPostButton({ ref }: { ref: RefObject<LexicalEditor | u
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const postId = queryParams.get("postId");
+  const [selectedFile, setSelectedFile] = useState<File|null>(null);
+  const [presignedData, setPresignedData] = useState<PresignedPostProps|null>(null);
 
   if (selector.category === undefined) {
     const category = matches[1].pathname.substring(1, matches[1].pathname.length);
@@ -44,7 +55,7 @@ export function PostSubmitPostButton({ ref }: { ref: RefObject<LexicalEditor | u
         title: selector.title,
         category: selector.category,
         content: content,
-        postId: postId,
+        postId: postId
       };
 
       const access_token = auth?.user?.access_token;
@@ -52,8 +63,8 @@ export function PostSubmitPostButton({ ref }: { ref: RefObject<LexicalEditor | u
         .post(CData.local_backend + "/update", postData, {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${access_token}`,
-          },
+            Authorization: `Bearer ${access_token}`
+          }
         })
         .then(async (r) => {
           await queryClient.invalidateQueries({ queryKey: [...queryKeys.PostDetail, postId] });
@@ -71,11 +82,10 @@ export function PostSubmitPostButton({ ref }: { ref: RefObject<LexicalEditor | u
       if (isEmpty(content)) return;
       if (matches.length < 2) return;
       clearImageSrcInEditorState(content);
-
       const postData = {
         title: selector.title,
         category: selector.category,
-        content: content,
+        content: content
       };
 
       const access_token = auth?.user?.access_token;
@@ -83,8 +93,8 @@ export function PostSubmitPostButton({ ref }: { ref: RefObject<LexicalEditor | u
         .post(CData.local_backend + "/save", postData, {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${access_token}`,
-          },
+            Authorization: `Bearer ${access_token}`
+          }
         })
         .then((r) => {
           navigate("/" + selector.category + "/" + r.data);
@@ -92,8 +102,60 @@ export function PostSubmitPostButton({ ref }: { ref: RefObject<LexicalEditor | u
     });
   }
 
+  function getPresignedPost() {
+    axios.get<PresignedPostProps>(CData.local_backend + "/get/presigned-post", {
+      headers: {
+        "Content-Type": "application/json",
+        "X-File-MimeType": selectedFile?.type,
+        Authorization: `Bearer ${auth.user?.access_token}`
+      }
+    }).then((r) => {
+      setPresignedData(r.data);
+    });
+  }
+
+  function getOnChange() {
+    return (event:BaseSyntheticEvent) => {
+      console.log(event)
+      console.log(event.target.files);
+      console.log("Hello");
+      setSelectedFile(event.target.files[0])
+    };
+  }
+
+  function postImage() {
+    const formData = new FormData();
+    if (presignedData?.fields == undefined) return;
+    const fields = presignedData?.fields;
+
+    const {key, ...rest} = fields;
+    formData.append('key', key);
+    Object.entries(rest).forEach(([key, value])=>{
+      formData.append(key, value)
+    })
+
+    if (!selectedFile) return;
+    formData.append('Content-Type', selectedFile.type)
+    formData.append('file', selectedFile)
+
+
+    axios.post(presignedData?.url, formData).then(r => console.log(r.status)).catch(a=>console.log(a));
+  }
+
   return (
     <>
+      <button className={styles.buttonApply}
+              onClick={getPresignedPost}
+      >Get Presigned Post
+      </button>
+      <input
+        type={"file"}
+        onChange={getOnChange()}
+      ></input>
+      <button className={styles.buttonApply}
+      onClick={()=>{postImage()}}>Post To Bucket Yeah
+      </button>
+
       {(postId === undefined || postId === null) && (
         <button className={styles.buttonApply} onClick={submitPost}>
           등록
