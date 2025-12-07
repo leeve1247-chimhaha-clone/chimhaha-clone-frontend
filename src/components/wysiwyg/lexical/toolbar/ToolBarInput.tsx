e4ddb90845f1203faa5172e28f93f8a6d0f0cada
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-12-07 17:44:13 +0900

    fix: update

--- src/components/wysiwyg/lexical/toolbar/ToolBarInput.tsx
+++ src/components/wysiwyg/lexical/toolbar/ToolBarInput.tsx
@@ -1,18 +1,37 @@
 import style from "./ToolbarPlugin.module.css";
-import React, {type ReactNode, useId} from "react";
+import { type ReactNode, useId } from "react";
+import { INSERT_IMAGE_COMMAND } from "../image/commands/INSERT_IMAGE_COMMAND.tsx";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 
-export function ToolBarInput({children, onChange}:{children:ReactNode, onChange: (e: React.ChangeEvent<HTMLInputElement>)=>void}){
-    const htmlForId = useId();
-    return (
-        <label htmlFor={htmlForId}>
-            <div className={`${style.toolbarItemButton}`}>
-                {children}
-                <input id={htmlForId}
-                       type="file"
-                       accept="image/*"
-                       className={style.hiddenInput}
-                       onChange={onChange}
-                />
-            </div></label>
-    )
+export function ToolBarInput({ children }: { children: ReactNode }) {
+  const htmlForId = useId();
+  const [editor] = useLexicalComposerContext();
+
+  function loadImage(files: FileList | null) {
+    const reader = new FileReader();
+    reader.onload = function() {
+      if (typeof reader.result === "string") {
+        editor.dispatchCommand(INSERT_IMAGE_COMMAND, {src: reader.result, altText:"123123", maxWidth:1500});
+      }
+    };
+    if (files !== null) {
+      reader.readAsDataURL(files[0]);
+    }
+  }
+
+  return (
+    <label htmlFor={htmlForId}>
+      <div className={`${style.toolbarItemButton}`}>
+        {children}
+        <input id={htmlForId}
+               type="file"
+               accept="image/*"
+               className={style.hiddenInput}
+               onChange={(e) => {
+                 loadImage(e.target.files);
+               }}
+        />
+      </div>
+    </label>
+  );
 }
